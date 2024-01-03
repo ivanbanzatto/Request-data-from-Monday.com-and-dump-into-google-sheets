d2328e5214a82cfbeb4efe9ad70d05a0efb7ddee
Ivan B <138217050+ivanbanzatto@users.noreply.github.com>
2024-01-03 10:52:10 +0000

    Update RequestToMonday_grapQL-3.js

--- RequestToMonday_grapQL-3.js
+++ RequestToMonday_grapQL-3.js
@@ -55,12 +55,6 @@ function requestMonday() {
 
 
 
-function clearSpreadsheet(){
-  //clear spreadsheet
-  SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Monday export").getRange("A1:FI10000").clearContent();
-  console.log("-= spreadsheet cleaned =-");
-}
-
 
 
 
